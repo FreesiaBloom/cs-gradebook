51700da7f815a15930ee9b6970b617ac75bca000
jasinaga <agata.jasinska215@gmail.com>
2019-06-13 14:20:35 +0200

    Passing param by reference

--- test/GradeBook.Tests/TypeTests.cs
+++ test/GradeBook.Tests/TypeTests.cs
@@ -5,8 +5,27 @@ namespace GradeBook.Tests
 {
     public class TypeTests
     {
+
         [Fact]
-        public void Test1()
+        public void CSharpCanPassByValue()
+        {
+            var book1 = GetBook("Book 1");
+            GetBookSetName(ref book1, "New Name");
+            
+            Assert.Equal("New Name", book1.Name);
+        }
+
+        [Fact]
+        public void CSharpIsPassByValue()
+        {
+            var book1 = GetBook("Book 1");
+            GetBookSetName(book1, "New Name");
+            
+            Assert.Equal("Book 1", book1.Name);
+        }
+
+        [Fact]
+        public void CanSetNameFromReference()
         {
             var book1 = GetBook("Book 1");
             SetName(book1, "New Name");
@@ -40,6 +59,16 @@ namespace GradeBook.Tests
         private void SetName(Book book1, string name)
         {
             book1.Name = name;
+        }
+
+        private void GetBookSetName(Book book, string name)
+        {
+            book = new Book(name);
+        }
+        
+        private void GetBookSetName(ref Book book, string name)
+        {
+            book = new Book(name);
         }
     }
 }
